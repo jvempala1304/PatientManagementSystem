7ecae4ecfbbda8c0519a6d5426bae4f875f53972
James Boby Vempala <jamesboby897@gmail.com>
2024-10-28 15:28:43 -0400

    Fixed UI

--- medisync/client/src/App.js
+++ medisync/client/src/App.js
@@ -1,10 +1,6 @@
-import React, { useEffect } from "react";
-import { generateToken } from "./firebase/firebase";
+import React from "react";
 
 function App() {
-  useEffect(() => {
-    generateToken();
-  }, []);
   return <h1>Medisync</h1>;
 }
 
